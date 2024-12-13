0699700d80c66cbf9ab22527ea9613da75116dab
Nataliq-Vulkova <nataliavalkova2009@gmail.com>
2024-12-13 10:34:15 +0200

    Add files via upload

---
+++ vse_taq/kasova_belejka/kasova_belejka.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kasova_belejka
+{
+    internal class kasova_belejka
+    {
+        static void top()
+        {
+             Console.WriteLine("Cash Receipt");
+            Console.WriteLine("---------------");
+        }
+        static void sredna()
+        { 
+            Console.WriteLine("Charged to____________");
+            Console.WriteLine("Received by_______________");
+        }
+        static void bottom()
+        {
+           Console.WriteLine("----------------");
+            Console.WriteLine("It kariera");
+        }
+        static void Main(string[] args)
+        {
+            top();
+            sredna();
+            bottom();
+            
+            
+        }
+    }
+}
---
+++ vse_taq/vse_taq/vse_taq.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vse_taq
+{
+    internal class vse_taq
+    {
+        static void PrintHeader()
+        {
+            Console.WriteLine("----------");
+            Console.WriteLine("Аз успях");
+            Console.WriteLine("Аз съм пълен бот");
+        }
+        static void Az(int a)
+        {
+            Console.WriteLine(a*a);
+        }
+        static void Main(string[] args)
+        {
+            PrintHeader();
+            Az(5);
+            Spasen(4, 2.2);
+        }
+        static void Spasen(int b, double c)
+        { 
+        double A=b*c;
+            Console.WriteLine("A= " + A);
+        }
+
+    }
+}
